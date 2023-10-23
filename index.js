e23904f1ba7b65c381a60647487c11092fe6fa29
Roshan Shah <85742866+roshanshah639@users.noreply.github.com>
2023-10-23 11:18:06 +0000

    data types

---
+++ index.js
@@ -0,0 +1,42 @@
+"use strict"; //treat all js code a newer version
+
+// const accountId = 144553;
+// console.log(accountId);
+// console.log(accountId);
+
+// let accountEmail = "roshan2gmail.com";
+// accountEmail = "hc@hc.com";
+
+// var accountPassword = "12345";
+// accountPassword = "123525";
+
+// accountCity = "Kathmandu";
+// accountCity = "Pokhara";
+
+// console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+// alert("Hello"); we are using node js not browser
+
+// console.log(3+3);
+
+// console.log("Hello");
+
+// Data Types
+
+// Primitives
+
+let name = "Roshan"; //string
+
+let age = 18; // number
+
+let a = null; // null standalone value
+
+let b = undefined;
+let c; // undefined
+
+let myletter = "B"; // symbol
+
+let isLoggedIn = false; // Boolean
+
+
+// Refrence
